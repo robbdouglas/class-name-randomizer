30b1689e829da967c6ba0386f77c630e97c227dc
robbdouglas <robert.nimmrich@icloud.com>
2023-10-17 16:32:40 +0200

    add checkboxes and bold task x elements

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Liste aller Studenten
+
 const students = [
   "Axel",
   "Robb",
@@ -16,31 +18,83 @@ const students = [
   "Hamze",
 ];
 
-function getRandomStudents(students, numberOfStudents) {
+// Funktion, die zufällige Studenten auswählt
+
+function getRandomStudents(students, numberOfStudents, excludedStudents) {
+  const availableStudents = students.filter(
+    (student) => !excludedStudents.includes(student)
+  );
+
   const randomStudents = [];
-  while (randomStudents.length < numberOfStudents) {
-    const randomStudent = students[Math.floor(Math.random() * students.length)];
-    if (!randomStudents.includes(randomStudent)) {
-      randomStudents.push(randomStudent);
-    }
+  while (
+    randomStudents.length < numberOfStudents &&
+    availableStudents.length > 0
+  ) {
+    const randomIndex = Math.floor(Math.random() * availableStudents.length);
+    const randomStudent = availableStudents.splice(randomIndex, 1)[0];
+    randomStudents.push(randomStudent);
   }
   return randomStudents;
 }
+
+// Funktion, die die Liste der Studenten rendert, d.h. in HTML umwandelt
+
+function renderStudentsList() {
+  const studentsList = document.getElementById("studentsList");
+  studentsList.innerHTML = "";
+
+  students.forEach((student) => {
+    const listItem = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.value = student;
+    listItem.appendChild(checkbox);
+    listItem.appendChild(document.createTextNode(student));
+    studentsList.appendChild(listItem);
+  });
+}
+
+// Event Listener, der auf den Button klickt und die Funktion ausführt
 
 document.getElementById("randomButton").addEventListener("click", function () {
   const numberOfStudents = parseInt(
     document.getElementById("numberOfStudents").value,
     10
+  ); 
+
+  const excludedStudents = Array.from(
+    document.querySelectorAll("#studentsList input:checked")
+  ).map((checkbox) => checkbox.value);
+
+  const result = getRandomStudents(
+    students,
+    numberOfStudents,
+    excludedStudents
   );
-  const result = getRandomStudents(students, numberOfStudents);
   const resultList = document.getElementById("resultList");
   resultList.innerHTML = "";
-  result.forEach((student) => {
+
+  // Ergebnis soll in HTML gerendert werden
+
+  result.forEach((student, index) => {
     const listItem = document.createElement("li");
-    listItem.textContent = student;
+    const strongElement = document.createElement("strong");
+    strongElement.textContent = `Task ${index + 1}: `;
+    listItem.appendChild(strongElement);
+    listItem.appendChild(document.createTextNode(student));
     resultList.appendChild(listItem);
   });
 });
 
+// Student List soll beim Laden der Seite gerendert werden
+renderStudentsList();
 
+//_______________________________________________________________________________________
 
+/* BONUS 
+
+1. all students must appear in browser with a checkbox. if checkbox next to the student is clicked, the student is excluded from the random selection
+2. wer dran kam wird nächste runde gesperrt
+3. speichern, nach refresh soll fortschritt gespeichert werden (local storage)
+
+*/
