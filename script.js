a76cf3c70db7d6f10ef64c736476a3ecc0bb42db
robbdouglas <robert.nimmrich@icloud.com>
2023-10-17 16:37:27 +0200

    add comments to explain code

--- script.js
+++ script.js
@@ -22,17 +22,17 @@ const students = [
 
 function getRandomStudents(students, numberOfStudents, excludedStudents) {
   const availableStudents = students.filter(
-    (student) => !excludedStudents.includes(student)
+    (student) => !excludedStudents.includes(student) // Wir filtern erst alle Studenten aus, die nicht ausgewählt werden sollen
   );
 
   const randomStudents = [];
   while (
     randomStudents.length < numberOfStudents &&
-    availableStudents.length > 0
+    availableStudents.length > 0 // Wir wählen so lange zufällige Studenten aus, bis die gewünschte Anzahl erreicht ist
   ) {
-    const randomIndex = Math.floor(Math.random() * availableStudents.length);
-    const randomStudent = availableStudents.splice(randomIndex, 1)[0];
-    randomStudents.push(randomStudent);
+    const randomIndex = Math.floor(Math.random() * availableStudents.length); // Wir wählen einen zufälligen Index aus
+    const randomStudent = availableStudents.splice(randomIndex, 1)[0]; // Wir wählen den Studenten an der Stelle des Index aus und entfernen ihn aus dem Array
+    randomStudents.push(randomStudent); // Wir fügen den Studenten dem Ergebnisarray der zufälligen Studenten hinzu
   }
   return randomStudents;
 }
@@ -41,16 +41,16 @@ function getRandomStudents(students, numberOfStudents, excludedStudents) {
 
 function renderStudentsList() {
   const studentsList = document.getElementById("studentsList");
-  studentsList.innerHTML = "";
+  studentsList.innerHTML = ""; // Wir leeren die Liste, damit sie nicht doppelt gerendert wird
 
   students.forEach((student) => {
-    const listItem = document.createElement("li");
-    const checkbox = document.createElement("input");
+    const listItem = document.createElement("li"); // Wir erstellen ein Listenelement
+    const checkbox = document.createElement("input"); // Wir erstellen ein Checkbox-Element
     checkbox.type = "checkbox";
     checkbox.value = student;
-    listItem.appendChild(checkbox);
-    listItem.appendChild(document.createTextNode(student));
-    studentsList.appendChild(listItem);
+    listItem.appendChild(checkbox); // Wir fügen die Checkbox dem Listenelement hinzu
+    listItem.appendChild(document.createTextNode(student)); // Wir fügen den Text dem Listenelement hinzu
+    studentsList.appendChild(listItem); // Wir fügen das Listenelement der Liste hinzu
   });
 }
 
@@ -60,29 +60,29 @@ document.getElementById("randomButton").addEventListener("click", function () {
   const numberOfStudents = parseInt(
     document.getElementById("numberOfStudents").value,
     10
-  ); 
+  ); // Wir holen uns die Anzahl der Studenten, die ausgewählt werden sollen
 
   const excludedStudents = Array.from(
     document.querySelectorAll("#studentsList input:checked")
-  ).map((checkbox) => checkbox.value);
+  ).map((checkbox) => checkbox.value); // Wir holen uns die Studenten, die ausgeschlossen werden sollen
 
   const result = getRandomStudents(
     students,
     numberOfStudents,
     excludedStudents
   );
-  const resultList = document.getElementById("resultList");
-  resultList.innerHTML = "";
+  const resultList = document.getElementById("resultList"); // Wir holen uns die Liste, in der das Ergebnis gerendert werden soll
+  resultList.innerHTML = ""; 
 
   // Ergebnis soll in HTML gerendert werden
 
   result.forEach((student, index) => {
     const listItem = document.createElement("li");
-    const strongElement = document.createElement("strong");
-    strongElement.textContent = `Task ${index + 1}: `;
-    listItem.appendChild(strongElement);
-    listItem.appendChild(document.createTextNode(student));
-    resultList.appendChild(listItem);
+    const strongElement = document.createElement("strong"); 
+    strongElement.textContent = `Task ${index + 1}: `; // Wir fügen die Aufgabe hinzu und erhöhen den Index um 1 (Aufgabe 1: ... Aufgabe 2: ...)
+    listItem.appendChild(strongElement); 
+    listItem.appendChild(document.createTextNode(student)); 
+    resultList.appendChild(listItem); 
   });
 });
 
